e84167c1f28628d93ea515ac0bf2adef10c4a5c5
Tomoya Sakai <>
2019-01-26 00:30:23 +0900

    add: group_scad

--- spmlib/thresholding/thresholding_operators.py
+++ spmlib/thresholding/thresholding_operators.py
@@ -32,6 +32,15 @@ def smoothly_clipped_absolute_deviation(z, th, a=3.7):
     scad[idx] = ((a - 1) * z[idx] - sgn(z[idx]) * a * th) / (a - 2)
     return scad
 
+def __smoothly_clipped_absolute_deviation(vz, vth, a=3.7):
+    scad = vz.copy()
+    absz = np.abs(vz)
+    idx = absz <= 2*vth
+    scad[idx] = soft(vz[idx], vth[idx])
+    idx = np.logical_and(absz > 2*vth, absz <= a*vth)
+    scad[idx] = ((a - 1) * vz[idx] - sgn(vz[idx]) * a * vth[idx]) / (a - 2)
+    return scad
+
 
 #%% hard thresholding
 def hard(z, th):
@@ -111,8 +120,8 @@ def group_soft(z, th, garray, normalize=True, thresholding=soft):
 
     m = z.size
     ng = np.max(garray)+1   # number of groups(subvectors)
-    normsz = np.zeros(ng)   # squared norms of subvectors
-    ms = np.zeros(ng)       # numbers of group members (dims. of subvectors)
+    normsz = np.zeros(ng, dtype=z.dtype)   # squared norms of subvectors
+    ms = np.zeros(ng, dtype=z.dtype)       # numbers of group members (dims. of subvectors)
     vecz = z.ravel()
     for i in range(m):
         g = garray.ravel()[i]
@@ -126,7 +135,7 @@ def group_soft(z, th, garray, normalize=True, thresholding=soft):
     # thresholding
     if normalize:
         #az = np.max(normzsn - np.sqrt(ms) * th / np.sqrt(m), 0.) / normzsn
-        az = thresholding(normzsn, np.sqrt(ms) * th / np.sqrt(m)) / normzsn
+        az = thresholding(normzsn, np.sqrt(ms) * (th / np.sqrt(m,dtype=ms.dtype))) / normzsn
     else:
         #az = np.max(normzsn - th, 0.) / normzsn
         az = thresholding(normzsn, th) / normzsn
@@ -142,4 +151,66 @@ def group_soft(z, th, garray, normalize=True, thresholding=soft):
 
 
 def group_scad(z, th, garray, normalize=True, a=3.7):
-    return group_soft(z, th, garray, normalize=normalize, thresholding=lambda x,t: smoothly_clipped_absolute_deviation(x,t,a=a))
+    """
+    Group-wise soft threshlding
+        soft(||z(g)||_2, `th`) / ||z(g)||_2 * z(g) for g in `groups`, 
+        i.e., `l2_soft(z[g], th)` with SCAD thresholding.
+    Here, `groups` is a list of index lists.
+    The l2 norm of each subvector z(g) shrinks by `th` without changing its direction.
+    The groups (entries of z) must not overlap.
+    If you have the list `groups`, make `garray` = index_groups_to_array(`groups`).
+
+    Note: this function is the proximity operator.
+        arg min_x sum_{g in `groups`}( th*||x(g)||_2 + 0.5*||x(g)-z(g)||_2^2 )
+
+    Parameters
+    ----------
+    z : array_like, shape (`m`,)
+        `m`-dimensional vector to be thresholded group-wise.
+    th : scalar,
+        Threshold.
+    garray : array_like, shape(`m`,)
+        `m`-dimensional vector of group IDs. There are max(garray)+1 groups.
+    normaize : bool, optional, default True
+        If True, the threshold th is scaled by sqrt(number of group members)/sqrt(m) for each subvector z(g).
+
+    Returns
+    -------
+    v : ndarray
+        The thresholded vector.
+    """
+
+    m = z.size
+    ng = np.max(garray)+1   # number of groups(subvectors)
+    normsz = np.zeros(ng, dtype=z.dtype)   # squared norms of subvectors
+    ms = np.zeros(ng, dtype=z.dtype)       # numbers of group members (dims. of subvectors)
+    vecz = z.ravel()
+    for i in range(m):
+        g = garray.ravel()[i]
+        normsz[g] += vecz[i]*vecz[i]
+        ms[g] += 1
+
+    # compute scales
+    normzsn = normsz.copy()
+    normzsn[normsz == 0.0] = 1.0    # to avoid zero-div
+    normzsn = np.sqrt(normzsn)
+    # thresholding
+    if normalize:
+        #az = np.max(normzsn - np.sqrt(ms) * th / np.sqrt(m), 0.) / normzsn
+        az = __smoothly_clipped_absolute_deviation(normzsn, np.sqrt(ms) * (th / np.sqrt(m,dtype=ms.dtype)),a) / normzsn
+    else:
+        #az = np.max(normzsn - th, 0.) / normzsn
+        az = smoothly_clipped_absolute_deviation(normzsn, th, a) / normzsn
+
+    # scaling
+    v = np.zeros_like(z)
+    vecv = v.ravel()
+    vecg = garray.ravel()
+    for i in range(m):
+        vecv[i] = vecz[i] * az[vecg[i]]
+
+    return v
+
+
+#def group_scad(z, th, garray, normalize=True, a=3.7):
+#    return group_soft(z, th, garray, normalize=normalize, thresholding=lambda x,t: __smoothly_clipped_absolute_deviation(x,t,a=a))
